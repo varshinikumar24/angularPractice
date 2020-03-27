8946880d5b650da3c405de191f0b0abcdd915538
varshini <ar.varshini@gmail.com>
2020-03-27 13:42:42 -0400

    updated Assignment 2

--- src/app/server2/server2.component.ts
+++ src/app/server2/server2.component.ts
@@ -9,21 +9,35 @@ export class Server2Component implements OnInit {
   serverDisabled = true;
   serverCreationStatus= "No server is created ";
   serverInput="test";
+  userName="";
+  userNameButton =true;
+  if(userName)
+  {
+    userName=false;
+  }
+
   constructor() {
-setTimeout(() => {
+  setTimeout(() => {
   this.serverDisabled =false;
-}, 2000);
-   }
+ 
+  if(this.userName != null){
+    this.userNameButton = false;}
+  }, 2000);
+  }
 
   ngOnInit(): void {
   }
 
+  
   getServerCreationstatus(){
-    this.serverCreationStatus="Server is created !!! ";
+    this.serverCreationStatus="Server is created !!! The server name is"+ this.serverInput;
   }
 
   onUpdate(event: Event){
       this.serverInput =(<HTMLInputElement>event.target).value;
+  }
+  onUserNameEntered(){
+     this.userName=" ";
   }
 
 
